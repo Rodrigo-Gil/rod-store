84c3f7dd3604e88cbd1068b39f04194c2d5e17d3
Rodrigo <gil00013@algonquinlive.com>
2021-05-14 10:35:19 -0400

    Created the sign up component

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -6,7 +6,6 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className='group'>
         <input className='form-input'
         onChange={handleChange} { ...otherProps }
-        type="text"
         />
         {
             label ?
--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './sign-in-sign-up.styles.scss';
-import SignIn from '../../components/sign-in/sign-in.components'
+import SignIn from '../../components/sign-in/sign-in.components';
+import SignUp from '../../components/sign-up/sign-up.components';
 
 const signInSignUpPage = () => (
     <div className='sign-in-sign-up'>
     <SignIn />
+    <SignUp />
     </div>
 );
 

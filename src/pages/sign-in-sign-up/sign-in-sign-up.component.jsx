e6ce106c9a9a9316f550fe6312116c61ed059b45
Rodrigo <gil00013@algonquinlive.com>
2021-05-18 20:51:02 -0400

    Update the app to redirect the logged in user to the home page

--- src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -4,11 +4,11 @@ import './sign-in-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.components';
 import SignUp from '../../components/sign-up/sign-up.components';
 
-const signInSignUpPage = () => (
+const SignInSignUpPage = () => (
     <div className='sign-in-sign-up'>
     <SignIn />
     <SignUp />
     </div>
 );
 
-export default signInSignUpPage
+export default SignInSignUpPage

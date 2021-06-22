48b7fe3836867baa3a0a34c2cd3cb03810348a10
Rodrigo <gil00013@algonquinlive.com>
2021-06-22 15:27:50 -0400

    Started creating the checkout page component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx'
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDoc } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/users.selectors';
@@ -46,6 +48,7 @@ class App extends React.Component {
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route exact path='/checkout' component={CheckoutPage} />
       <Route exact path='/signin' render={ () => this.props.currentUser ?
         (<Redirect to='/' />
           ) : (

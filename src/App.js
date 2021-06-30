7bdbd6b0f119f41777bbdec15a9bb071d728db1d
Rodrigo <gil00013@algonquinlive.com>
2021-06-30 09:43:00 -0400

    Created the contact page component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import ContactPage from './pages/contact/contact.component.jsx';
 import Header from './components/header/header.component.jsx'
 import SignInSignUpPage from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
@@ -48,6 +49,7 @@ class App extends React.Component {
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/contact' component={ContactPage} />
       <Route exact path='/checkout' component={CheckoutPage} />
       <Route exact path='/signin' render={ () => this.props.currentUser ?
         (<Redirect to='/' />
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './contact.styles.scss';
+
+const ContactPage = () => (
+    <div className='contact-page'>
+        <h2 className='title'>
+            Page under construction
+        </h2>
+    </div>
+)
+
+export default ContactPage;

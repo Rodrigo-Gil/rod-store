029e487a1fa431d0ec76d183286ae03db0abe06b
Rodrigo <gil00013@algonquinlive.com>
2021-07-28 12:27:29 -0400

    Refactored collection-preview component to redirect user to the collections page

--- src/components/collection-preview/collection-preview.styles.jsx
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -1,17 +1,22 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const CollectionPreviewContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 30px;
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 30px;
+`;
 
-    h1 {
-        font-size: 28px;
-        margin-bottom: 25px;
-    }
+export const TitleContainer = styled.h1`
+	cursor: pointer;
+	font-size: 28px;
+	margin-bottom: 25px;
+
+	&:hover {
+		color: grey;
+	}
 `;
 
 export const PreviewItem = styled.div`
-    display: flex;
-    justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
